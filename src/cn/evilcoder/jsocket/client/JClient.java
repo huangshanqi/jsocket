112c5001c45a658a5125bc2424225f000e112e96
huangshanqi <huangshanqi@neotel.co,.cn>
2016-03-13 11:53:12 +0800

    project initial

---
+++ src/cn/evilcoder/jsocket/client/JClient.java
@@ -0,0 +1,9 @@
+package cn.evilcoder.jsocket.client;
+
+/**
+ * User: Huangshanqi
+ * Date: 2016/3/13
+ * Time: 11:52
+ */
+public class JClient {
+}
---
+++ src/cn/evilcoder/jsocket/server/JServer.java
@@ -0,0 +1,9 @@
+package cn.evilcoder.jsocket.server;
+
+/**
+ * User: Huangshanqi
+ * Date: 2016/3/13
+ * Time: 11:51
+ */
+public class JServer {
+}
